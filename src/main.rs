d14e0ad7c4da307058eba40763d2297d3fd23bb6
HimbeerserverDE <himbeerserverde@gmail.com>
2022-12-16 15:16:53 +0100

    basic arg acceptor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author = "The Rustkrazy Authors", version = "v0.1.0", about = "Generate a rustkrazy image.", long_about = None)]
+struct Args {
+    /// Output location of a full image.
+    #[arg(short = 'o', long = "overwrite")]
+    overwrite: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("overwriting {}", args.overwrite);
 }
